7cff74f171e887486bd6297cd1bf129b3d7e02b2
Peckins <messias.morais@etec.sp.gov.br>
2019-03-18 20:17:29 +0000

    correção de script javascript e linkagem de páginas php

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,18 +1,39 @@
 //Verificando opção selecionada
-$(document).on("click", "#enviar", function(){
-   var selecao = $("#selecao").val();//pega o valor do select
+/*$(document).on("submit", "#enviar", function(){
    
-   if (selecao == "1"){
-       document.formulario.action = "exercicio.php";
-       //document.formulario.submit();
-   }
-   else if (selecao == "2"){
-       document.formulario.action = "#";
-       //document.formulario.submit();
-   }
-   else{
-       document.formulario.action = "#";
-       //document.formulario.submit();
+   var selecao = $("#selecao").on("change");
+   
+   if (selecao != ""){
+      
+      //Redirecionando o usuário para a página selecionada
+      if (selecao == "1"){
+         document.formulario.action = "exercicio1.php";
+      }
+      else if (selecao == "2"){
+         document.formulario.action = "exercicio2.php";
+      }
+      else{
+         document.formulario.action = "exercicio3.php";
+      }
    }
    
-});
+});*/
+
+function verificaSelecao(){
+   
+   var selecao = $("#selecao").val();
+   
+   if (selecao != ""){
+      
+      //Redirecionando o usuário para a página selecionada
+      if (selecao == "1"){
+         document.formulario.action = "exercicio1.php";
+      }
+      else if (selecao == "2"){
+         document.formulario.action = "exercicio2.php";
+      }
+      else{
+         document.formulario.action = "exercicio3.php";
+      }
+   }
+}
